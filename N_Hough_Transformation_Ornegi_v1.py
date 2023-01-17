1d9fd61002d181bb43ee600f68632c117701fe10
Riptide AUV <120055921+riptideauv@users.noreply.github.com>
2023-01-17 15:21:42 +0300

    Update N_Hough_Transformation_Ornegi_v1.py

--- N_Hough_Transformation_Ornegi_v1.py
+++ N_Hough_Transformation_Ornegi_v1.py
@@ -3,8 +3,8 @@ import numpy as np
 
 #Örnek bir koddur. Kenar tespiti algoritması kullanılmamıştır. Bu algoritmada sadece renk maskelemesi üzerinden bir tespit uygulanmıştır.
 
-frameWidth = 640
-frameHeight = 480
+frameWidth = 500
+frameHeight = 350
 cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
@@ -14,26 +14,25 @@ while(True):
     success, img1 = cap.read()
     img1 = cv2.flip(img1, 1)
     img2 = img1.copy()
-    #img1 = cv2.imread("coins.jpg")
 
     gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     hsv = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
 
-    kirmizi_min = np.array([0, 157, 184], np.uint8)
-    kirmizi_max = np.array([180, 255, 255], np.uint8)
+    sari_min = np.array([20, 100, 100])
+    sari_max = np.array([158, 255, 255])
 
-    maske = cv2.inRange(hsv, kirmizi_min, kirmizi_max)
+    maske = cv2.inRange(hsv, sari_min, sari_max)
 
     img1_blur = cv2.medianBlur(maske, 5)#pozitif ve tek olmalı 2. parametre
 
     circles = cv2.HoughCircles(img1_blur, cv2.HOUGH_GRADIENT, 1, img1.shape[0]/0.1, param1=200, param2=10, minRadius=5, maxRadius=30)
     #(kaynak resim, yöntem, çözünürlük ölçeği,çemberler arası mesafe img1.shape[0]/64 ile oynayıp değiştirebilirsin...)
 
-    cv2.rectangle(img2, (320 - 30, 240 - 30), (320 + 30, 240 + 30), (0, 0, 0),thickness=2)  # Kordinat Sistemi Merkezi Hedef Alanı
-    cv2.line(img2, (320, 240 - 30), (320, 0), (0, 0, 0), thickness=2)  # Kordinat Sistemim Çizgileri
-    cv2.line(img2, (320, 240 + 30), (320, 480), (0, 0, 0), thickness=2)
-    cv2.line(img2, (320 - 30, 240), (0, 240), (0, 0, 0), thickness=2)
-    cv2.line(img2, (320 + 30, 240), (640, 240), (0, 0, 0), thickness=2)
+    cv2.circle(img2,(320,200),30,(0,0,0),thickness=2)  # Kordinat Sistemi Merkezi Hedef Alanı
+    cv2.line(img2, (320, 240 - 70), (320, 0), (0, 0, 0), thickness=2)  # Kordinat Sistemim Çizgileri
+    cv2.line(img2, (320, 240 - 10), (320, 480), (0, 0, 0), thickness=2)
+    cv2.line(img2, (240 + 50, 200), (0, 200), (0, 0, 0), thickness=2)
+    cv2.line(img2, (320 + 30, 200), (640, 200), (0, 0, 0), thickness=2)
 
     if circles is not None:
         circles = np.uint16(np.around(circles))#around ile değerleri yuvarladık
@@ -41,29 +40,24 @@ while(True):
             #buradaki 0. ve  1. değerler {(a,b), koordinat sistemindeki merkez noktasının konumu} merkez noktasının koordinatı. 2. değer ise yarıçaptır.(r)
             cv2.circle(img2,(i[0], i[1]), i[2],(0,255,0),2)
 
-            if i[0] > 290 and i[0] < 350 and i[1] > 210 and i[1] < 270:
+            if i[0] > 300 and i[0] < 340 and i[1] > 180 and i[1] < 240:
 
-                cv2.putText(img2, "KILITLENDI!:", (i[0], i[1] + i[2]), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
-                cv2.putText(img2, "X:" + str(i[0]) + "  Y:" + str(i[1]), (i[0], i[1] + i[2] + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
+                cv2.putText(img2, "KILITLENDI!:", (i[0], i[1] + i[2]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0,0), 1)
+                cv2.putText(img2, "X:" + str(i[0]) + "  Y:" + str(i[1]), (i[0], i[1] + i[2] + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 1)
 
-                cv2.rectangle(img2, (320 - 30, 240 - 30), (320 + 30, 240 + 30), (0, 0, 255),thickness=2)  # Kordinat Sistemi Merkezi Hedef Alanı
-                cv2.line(img2, (320, 240 - 30), (320, 0), (0, 0, 255), thickness=2)  # Kordinat Sistemim Çizgileri
-                cv2.line(img2, (320, 240 + 30), (320, 480), (0, 0, 255), thickness=2)
-                cv2.line(img2, (320 - 30, 240), (0, 240), (0, 0, 255), thickness=2)
-                cv2.line(img2, (320 + 30, 240), (640, 240), (0, 0, 255), thickness=2)
+                cv2.circle(img2,(320,200),30,(0,0,0),thickness=2)  # Kordinat Sistemi Merkezi Hedef Alanı
+                cv2.line(img2, (320, 240 - 70), (320, 0), (0, 0, 0), thickness=2)  # Kordinat Sistemim Çizgileri
+                cv2.line(img2, (320, 240 - 10), (320, 480), (0, 0, 0), thickness=2)
+                cv2.line(img2, (240 + 50, 200), (0, 200), (0, 0, 0), thickness=2)
+                cv2.line(img2, (320 + 30, 200), (640, 200), (0, 0, 0), thickness=2)
             else:
 
                 cv2.putText(img2, "KORDINAT:", (i[0], i[1]+i[2]), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
                 cv2.putText(img2, "X:" + str(i[0]) + "  Y:" + str(i[1]), (i[0], i[1]+i[2]+10),cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
 
-    #cv2.namedWindow("Kirmizi Daire", cv2.WND_PROP_FULLSCREEN)
-    #cv2.setWindowProperty("Kirmizi Daire", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-
-
-    cv2.imshow("Kirmizi Daire Maske", maske)
-    cv2.imshow("Kirmizi Daire", img2)
-    #cv2.resizeWindow("Cirmizu Dayire", 1366, 768)
+ 
+    cv2.imshow("Cember Maske", maske)
+    cv2.imshow("Sari Cember", img2)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
